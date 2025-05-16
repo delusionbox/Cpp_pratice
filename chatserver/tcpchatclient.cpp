8a2e44acfe0437fa9049d0b0e9316fdf17aaf6b8
Docofarm <changhyeonseo283@gmail.com>
2025-05-17 00:19:49 +0900

    chatserve and client second

--- chatserver/tcpchatclient.cpp
+++ chatserver/tcpchatclient.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
+#include <thread>
+#include <string>
 #include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
 
+void receiveMessage(SOCKET sock)
+{
+    char buffer[1024];
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytesReceived > 0)
+        {
+            cout << "\nserver" << buffer << endl;
+        }
+    }
+}
+
 int main()
 {
     WSADATA wsa;
-    SOCKET sock;
-    sockaddr_in server;
-    char message[1024] = "Hello Server!";
-
     WSAStartup(MAKEWORD(2, 2), &wsa);
-    sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_port = htons(8888);
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_port = htons(8888);
 
-    connect(sock, (sockaddr *)&server, sizeof(server));
-    send(sock, message, sizeof(message), 0);
+    if (connect(sock, (sockaddr *)&serverAddr, sizeof(serverAddr)) == 0)
+    {
+        cout << "Connected Server!";
+
+        thread recvThread(receiveMessage, sock);
+        recvThread.detach();
+
+        char message[1024];
+        while (true)
+        {
+            cin.getline(message, sizeof(message));
+            send(sock, message, strlen(message), 0);
+        }
+    }
 
     closesocket(sock);
     WSACleanup();
-
     return 0;
 }
--- chatserver/tcpchatserver.cpp
+++ chatserver/tcpchatserver.cpp
@@ -1,35 +1,69 @@
 #include <iostream>
-#include <WinSock2.h>
+#include <thread>
+#include <vector>
+#include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib") // winsock library
 using namespace std;
 
+vector<SOCKET> clients;
+
+void handle_client(SOCKET clientSocket)
+{
+    char buffer[1024];
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytesRecevied = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (bytesRecevied <= 0)
+        {
+            cout << "Client disconnect\n";
+            closesocket(clientSocket);
+            return;
+        }
+
+        cout << "Client: " << buffer << endl;
+
+        // all client send message
+        for (SOCKET client : clients)
+        {
+            if (client != clientSocket)
+            {
+                send(client, buffer, bytesRecevied, 0);
+            }
+        }
+    }
+}
+
 int main()
 {
+
     WSADATA wsa;
-    SOCKET serverSocket, clientSocket;
-    sockaddr_in server, client;
-    char buffer[1024] = {0};
-
     WSAStartup(MAKEWORD(2, 2), &wsa);
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8888);
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(8888);
 
-    bind(serverSocket, (sockaddr *)&server, sizeof(server));
-    listen(serverSocket, 3);
+    bind(serverSocket, (sockaddr *)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, 10);
+    cout << "server listen 8888 port...\n";
 
-    int clientSize = sizeof(client);
-    clientSocket = accept(serverSocket, (sockaddr *)&client, &clientSize);
+    while (true)
+    {
+        sockaddr_in clientAddr;
+        int addrLen = sizeof(clientAddr);
+        SOCKET clientSocket = accept(serverSocket, (sockaddr *)&clientAddr, &addrLen);
+        cout << "New Client Connect!\n";
 
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Client: " << buffer << endl;
+        clients.push_back(clientSocket);
+        thread clientThread(handle_client, clientSocket);
+        clientThread.detach();
+    }
 
-    closesocket(clientSocket);
     closesocket(serverSocket);
     WSACleanup();
-
     return 0;
 }
